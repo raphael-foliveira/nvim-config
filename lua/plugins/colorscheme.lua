c6cf462079707cc0474e00d6ab2aeb16638b3859
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-04-06 22:25:30 -0300

    set copilot as disabled by default

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,32 +1,35 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "night" },
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      contrast = "hard",
-    },
-  },
-  { "Mofiqul/vscode.nvim" },
-  { "projekt0n/github-nvim-theme" },
-  { "Shatur/neovim-ayu" },
-  { "doums/darcula" },
-  {
-    "navarasu/onedark.nvim",
-    opts = {
-      style = "darker",
-      toggle_style_key = "<leader>t",
-      toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" },
-    },
-  },
-  { "catppuccin/nvim" },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "onedark",
     },
+  },
+  {
+    name = "catppuccin",
+    "catppuccin/nvim",
+  },
+  { "Mofiqul/vscode.nvim" },
+  {
+    "navarasu/onedark.nvim",
+    name = "onedark",
+    opts = {
+      style = "cool",
+      toggle_style_key = "<leader>t",
+      toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" },
+      colors = {
+        black = "#080808",
+        bg0 = "#080808",
+        bg1 = "#202020",
+        bg2 = "#080808",
+        bg3 = "#080808",
+        bg_d = "#080808",
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    opts = { style = "night" },
   },
 }
