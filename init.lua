07b7a9b3ab4085875e67361c3a17fed2cdddbafb
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-05-20 08:37:43 -0300

    disable copilot on startup by default

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-if vim.g.vscode then
-  print("running inside vscode")
-end
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  command = "Copilot disable",
+})
