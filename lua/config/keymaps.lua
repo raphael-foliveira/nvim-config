2be17a353b6de51701e6701d4972876911635edd
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-06-25 19:24:54 -0300

    cleanup and remove comments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
---
-
 local function combined_c_c()
   vim.cmd("nohl")
   vim.fn["copilot#Dismiss"]()
