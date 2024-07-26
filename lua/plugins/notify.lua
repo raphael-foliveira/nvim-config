1b03262f668581ad625412b07185ca22fe44e6ee
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-07-26 18:02:47 -0300

    create notify options object

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,3 +1,16 @@
+local options = {
+  stages = "static",
+  timeout = 3000,
+  max_height = function()
+    return math.floor(vim.o.lines * 0.75)
+  end,
+  max_width = function()
+    return math.floor(vim.o.columns * 0.75)
+  end,
+  on_open = function(win)
+    vim.api.nvim_win_set_config(win, { zindex = 100 })
+  end,
+}
 return {
   "rcarriga/nvim-notify",
   keys = {
@@ -19,10 +32,12 @@ return {
         local notify = require("notify")
         if vim.g.notify_enabled then
           notify("Disabling notifications...")
-          notify.setup({ level = "off" })
+          options.level = "off"
+          notify.setup(options)
           vim.g.notify_enabled = false
         else
-          notify.setup({ level = "info" })
+          options.level = "info"
+          notify.setup(options)
           vim.g.notify_enabled = true
           notify("Notifications are now enabled", "info")
         end
