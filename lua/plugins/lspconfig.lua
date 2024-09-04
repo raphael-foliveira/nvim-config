440cc4df33a29660eb0e976cc78691b351da4432
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-09-04 10:46:03 -0300

    lsp config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,5 +2,14 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    vtsls = {
+      keys = {
+        {
+          "<leader>co",
+          LazyVim.lsp.action["source.removeUnused.ts"],
+          desc = "Organize Imports",
+        },
+      },
+    },
   },
 }
